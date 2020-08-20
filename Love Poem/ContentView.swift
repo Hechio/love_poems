a3ed3c99999862f00e9b69bed2198db49cd163e9
Steve Hechio <stevehechio@gmail.com>
2020-08-20 20:38:25 +0300

    Initial Commit

---
+++ Love Poem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Love Poem
+//
+//  Created by Joel Personal on 8/20/20.
+//  Copyright © 2020 Steve Hechio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
